5afef14b24181ad67758dd45d3eac6c9b4f9b859
zhangyang <zy@zhangyangs-MacBook-Pro.local>
2021-01-03 15:32:50 +0800

    Keep window size

--- src/app/app_window.cpp
+++ src/app/app_window.cpp
@@ -52,6 +52,7 @@ namespace app {
         if (!glfwInit())
             exit(EXIT_FAILURE);
         
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
